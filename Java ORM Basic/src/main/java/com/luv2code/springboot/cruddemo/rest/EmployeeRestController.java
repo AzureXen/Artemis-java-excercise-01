f94e53fb355fbd5c2b117d0bd2f29d84ed809a09
AzureXen <phamdanminh@gmail.com>
2024-09-21 19:27:11 +0700

    yeah

---
+++ Java ORM Basic/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,29 @@
+package com.luv2code.springboot.cruddemo.rest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+    //use Employee Service
+
+
+
+    //expose "/employees" and return a list of employees
+
+
+
+
+    //expose "/employees/{employeeId}" and return a single employee by id
+
+
+
+
+    //expose "/employees" to add a student with Post mapping
+
+
+
+
+
+}
